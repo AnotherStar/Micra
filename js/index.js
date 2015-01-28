6ae505b6c578f313359b06db818fc17eddda5025
AnotherStar <AliumAstrum@gmail.com>
2015-01-28 23:51:47 +0400

    3

--- js/index.js
+++ js/index.js
@@ -41,7 +41,7 @@ var app = {
         disconnectButton.ontouchstart = app.disconnect;
         // listen for messages
         bluetoothSerial.subscribe("\n", app.onmessage, app.generateFailureFunction("Subscribe Failed"));
-        bluetoothSerial.subscribe("S11"), function(message){
+        bluetoothSerial.subscribe("S11", function(message){
         	alert('message S11 ' + message);
         });
         // get a list of peers
