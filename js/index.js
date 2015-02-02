032f87521bc1748748682a34da55008756ef7bce
AnotherStar <AliumAstrum@gmail.com>
2015-02-03 00:05:18 +0400

    7

--- js/index.js
+++ js/index.js
@@ -1,17 +1,3 @@
-/*
-Chat Example for Bluetooth Serial PhoneGap Plugin
-http://github.com/don/BluetoothSerial
-Copyright 2013 Don Coleman
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 /* jshint quotmark: false, unused: vars */
 /* global cordova, bluetoothSerial, listButton, connectButton, sendButton, disconnectButton */
 /* global chatform, deviceList, message, messages, statusMessage, chat, connection */
@@ -132,7 +118,8 @@ var app = {
         connection.style.display = "none";
         chat.style.display = "block";
         app.setStatus("Connected");
-        $('#menu').hide();
+        $('#connection').addClass('hidden');
+       // $('#menu').hide();
     },
     ondisconnect: function(reason) {
         var details = "";
@@ -143,7 +130,9 @@ var app = {
         app.enable(connectButton);
         chat.style.display = "none";
         app.setStatus("Disconnected");
-        $('#menu').show();
+        //$('#menu').show();
+        $('#connection').animate({height: 0}, 2000);
+
     },
     onmessage: function(message) {
     	alert('1 ' + message);
